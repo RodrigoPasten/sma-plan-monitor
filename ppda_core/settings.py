9b9e2e471cc70df383ca05c39fca9f1be0960014
RodrigoPasten <rodrigo.pasten.c@gmail.com>
2025-04-19 15:08:37 -0400

    fix: update dj-database-url to support conn_max_age

--- ppda_core/settings.py
+++ ppda_core/settings.py
@@ -33,7 +33,7 @@ dbport = os.getenv('DB_PORT')
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'tu-clave-secreta-de-desarrollo')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
